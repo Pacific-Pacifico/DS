1573c7abb9059b6bf2a88ed562a9fb1169b236ae
Pacific-Pacifico <30399370+Pacific-Pacifico@users.noreply.github.com>
2018-02-07 17:25:33 +0530

    doubly list main() created

--- doubly_linked_list_all_in_one.c
+++ doubly_linked_list_all_in_one.c
@@ -1,1 +1,50 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *prev;
+    struct node *next;
+}node;
+
+node *create(node *h);
+
+int main()
+{
+node *h;
+int op,op1;
+while(1)
+{
+    printf("\n\nselect the required option");
+    printf("\n1.create list");
+    printf("\n2.display");
+    printf("\n3.insert");
+    printf("\n4.delete");
+    printf("\n5.exit");
+    scanf("%d",&op);
+    switch(op)
+    {
+        case 1:
+            h=create(h);
+            break;
+
+        case 2:
+            break;
+
+        case 3:
+            break;
+
+        case 4:
+            break;
+
+        case 5:
+            exit(1);
+
+        default:
+            printf("\ninvalid selection");
+    }
+}
+}
 
